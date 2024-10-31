90a29d25c47dbc4c82dc285534b4d5c6d849c4d7
jskumaar <jskumaar@umich.edu>
2024-10-30 20:07:47 -0400

    Dynamic URL updates fixes

--- simple_game_test/app/__init__.py
+++ simple_game_test/app/__init__.py
@@ -14,7 +14,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# app.config['APPLICATION_ROOT'] = '/flask_closed_loop_teaching/'  # Comment this line when running on local host to avoid CSRF token error
+app.config['APPLICATION_ROOT'] = '/flask_closed_loop_teaching/'  # Comment this line when running on local host to avoid CSRF token error
 # # app.config['SESSION_COOKIE_PATH'] = '/flask_closed_loop_teaching'
 # # app.config['SESSION_COOKIE_SECURE'] = True
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1) # Apply ProxyFix middleware for subroutes in externalnginx server
