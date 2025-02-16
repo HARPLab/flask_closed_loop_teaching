8cc093ec4275518ab131564c22af1c707acd8e66
jskumaar <jskumaar@umich.edu>
2025-02-15 22:06:10 -0500

    Flask fixes - try 2

--- simple_game_test/app/__init__.py
+++ simple_game_test/app/__init__.py
@@ -14,7 +14,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# app.config['APPLICATION_ROOT'] = '/flask_closed_loop_teaching'  # Comment this line when running on local host to avoid CSRF token error
+app.config['APPLICATION_ROOT'] = '/flask_closed_loop_teaching'  # Comment this line when running on local host to avoid CSRF token error
 # app.config['SESSION_COOKIE_PATH'] = '/flask_closed_loop_teaching'
 # app.config['SESSION_COOKIE_SECURE'] = True
 # app.config['PREFERRED_URL_SCHEME'] = 'https'
