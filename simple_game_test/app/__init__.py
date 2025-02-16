95e26262d9467e86e27ab2093f34f06d73491fb3
jskumaar <jskumaar@umich.edu>
2025-02-15 21:50:57 -0500

    Flask fixes - try 1

--- simple_game_test/app/__init__.py
+++ simple_game_test/app/__init__.py
@@ -14,11 +14,11 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['APPLICATION_ROOT'] = '/flask_closed_loop_teaching'  # Comment this line when running on local host to avoid CSRF token error
-app.config['SESSION_COOKIE_PATH'] = '/flask_closed_loop_teaching'
-app.config['SESSION_COOKIE_SECURE'] = True
-app.config['PREFERRED_URL_SCHEME'] = 'https'
-app.static_url_path = '/flask_closed_loop_teaching/static'
+# app.config['APPLICATION_ROOT'] = '/flask_closed_loop_teaching'  # Comment this line when running on local host to avoid CSRF token error
+# app.config['SESSION_COOKIE_PATH'] = '/flask_closed_loop_teaching'
+# app.config['SESSION_COOKIE_SECURE'] = True
+# app.config['PREFERRED_URL_SCHEME'] = 'https'
+# app.static_url_path = '/flask_closed_loop_teaching/static'
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1) # Apply ProxyFix middleware for subroutes in externalnginx server
 # app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=2) # Apply ProxyFix middleware for subroutes in externalnginx server (after update?)
 
