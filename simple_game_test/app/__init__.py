e7db83b71beb570faff9e9e76f970097e7024403
jskumaar <jskumaar@umich.edu>
2025-02-18 13:05:47 -0500

    Flask-nginx fixes try 6

--- simple_game_test/app/__init__.py
+++ simple_game_test/app/__init__.py
@@ -14,7 +14,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# app.config['APPLICATION_ROOT'] = '/flask_closed_loop_teaching'  # Comment this line when running on local host to avoid CSRF token error
+app.config['APPLICATION_ROOT'] = '/flask_closed_loop_teaching'  # Comment this line when running on local host to avoid CSRF token error
 # app.config['SESSION_COOKIE_PATH'] = '/flask_closed_loop_teaching' # default is APPLICATION_ROOT
 # app.config['SESSION_COOKIE_SECURE'] = True
 # app.config['PREFERRED_URL_SCHEME'] = 'https'
@@ -39,7 +39,7 @@ login.login_view = "login"
 
 socketio = SocketIO(app)  # for running on local host
 # socketio.init_app(app)
-# socketio = SocketIO(app, path='/flask_closed_loop_teaching/socket.io')
+socketio = SocketIO(app, path='/flask_closed_loop_teaching/socket.io')
 
 
 # if __name__ == '__main__':
