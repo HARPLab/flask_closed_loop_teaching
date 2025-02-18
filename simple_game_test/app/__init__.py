013dfbac945f9a4f58c64b4664a960063acaee58
jskumaar <jskumaar@umich.edu>
2025-02-18 18:54:55 -0500

    Flask-nginx fixes try 11

--- simple_game_test/app/__init__.py
+++ simple_game_test/app/__init__.py
@@ -16,10 +16,12 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.config['APPLICATION_ROOT'] = '/flask_closed_loop_teaching'  # Comment this line when running on local host to avoid CSRF token error
 app.config['FORCE_SCRIPT_NAME'] = '/flask_closed_loop_teaching' # Comment this line when running on local host to avoid CSRF token error
+app.config['SESSION_COOKIE_SECURE'] = True  # Needed if running on HTTPS, 
+app.config['PREFERRED_URL_SCHEME'] = 'https'
+
 # app.config['SESSION_COOKIE_PATH'] = '/flask_closed_loop_teaching' # default is APPLICATION_ROOT
-# app.config['SESSION_COOKIE_SECURE'] = True
-# app.config['PREFERRED_URL_SCHEME'] = 'https'
-# app.static_url_path = '/flask_closed_loop_teaching/static'
+# app.static_url_path = '/flask_closed_loop_teaching/static'  # default is APPLICATION_ROOT/static
+
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1) # Apply ProxyFix middleware for subroutes in externalnginx server
 # app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=2) # Apply ProxyFix middleware for subroutes in externalnginx server (after update?)
 
@@ -41,7 +43,7 @@ login.login_view = "login"
 socketio = SocketIO(app)  # for running on local host
 # socketio.init_app(app)
 
-socketio = SocketIO(app, path='/flask_closed_loop_teaching/socket.io', cors_allowed_origins="*")  # Allow cross-origin for local testing
+# socketio = SocketIO(app, path='/flask_closed_loop_teaching/socket.io', cors_allowed_origins="*")  # Allow cross-origin for local testing
 
 
 # if __name__ == '__main__':
