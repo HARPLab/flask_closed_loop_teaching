51dbaa400d9e30f386fd599abf4b9c5dd98794d2
jskumaar <jskumaar@umich.edu>
2025-03-27 07:29:57 -0400

    Study mode

--- simple_game_test/app/routes.py
+++ simple_game_test/app/routes.py
@@ -76,7 +76,7 @@ CARD_ID_TO_FEATURES = [
 ]
 '''
 
-QUICK_DEBUG_FLAG = True
+QUICK_DEBUG_FLAG = False
 
 # Timeout for reconnection (in seconds)
 RECONNECT_TIMEOUT = 180  # Change this to the desired time
