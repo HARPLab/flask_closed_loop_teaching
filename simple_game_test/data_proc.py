f39aa35acc34385d07db2dc3f9b710fbe53c89dc
jskumaar <jskumaar@umich.edu>
2025-04-02 12:28:37 -0400

    Audio files path

--- simple_game_test/data_proc.py
+++ simple_game_test/data_proc.py
@@ -248,7 +248,7 @@ def plot_prob_learning():
 
 
 # Replace 'app.db' with your database file and 'output.xlsx' with the desired Excel file name
-convert_db_to_excel('app.db', 'output_debug_feb13.xlsx')
+convert_db_to_excel('app_bf_03_25.db', 'output_debug_mar25.xlsx')
 
 # print_table_data('app_bf_jan30.db')
 
