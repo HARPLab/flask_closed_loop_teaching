ed57e19d5718b15a2b4781e3ee097bfaa051ca6a
jskumaar <jskumaar@umich.edu>
2025-06-11 22:52:38 -0400

    Refactor settings 9

--- simple_game_test/app/routes.py
+++ simple_game_test/app/routes.py
@@ -1042,7 +1042,7 @@ def settings(data):
     # else:
 
     
-    if data["domain"] is not None and current_user.iteration == data["iteration"]: 
+    if data["domain"] is None or current_user.iteration == data["iteration"]: 
         repeating_data = False
     else:
         repeating_data = True
