f258abf585bd31085aabf9f4e87a339d5655b28a
jskumaar <jskumaar@umich.edu>
2025-06-24 18:42:13 -0400

    Refactoring settings function 11

--- simple_game_test/app/routes.py
+++ simple_game_test/app/routes.py
@@ -1772,7 +1772,7 @@ def consent():
     if IS_IN_PERSON:
         procedure = "This study may take up to 90 minutes, and audio/screen recordings will be collected."
     else:
-        procedure = "This study may take up to 60 minutes."
+        procedure = "This study may take up to 70 minutes."
     return render_template("consent.html", title="Consent", form=form, procedure=procedure)
 
 
